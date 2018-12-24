1e2579387f3b5bcdc66faf6ada62f050c8b5ecb5
Pascal Precht <pascal.precht@gmail.com>
2018-12-24 13:09:52 +0100

    refactor: read package author data from Cargo.toml inside CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,11 +5,12 @@ use clap::App;
 
 fn main() {
   let pkg: toml::Value = toml::from_str(include_str!("../Cargo.toml")).unwrap();
-  let version = pkg["package"]["version"].as_str().expect("'version' has to be a string!");
+  let version = pkg["package"]["version"].as_str().unwrap();
+  let authors = pkg["package"]["authors"].as_array().unwrap();
 
   App::new("Vibranium CLI")
           .version(version)
-          .author("Pascal Precht <pascal.precht@gmail.com>")
+          .author(authors[0].as_str().unwrap())
           .about("Building DApps made easy")
           .get_matches();
 }
