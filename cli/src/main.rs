231b24db08a0e80d1cda50d1884e274b96b847a3
Pascal Precht <pascal.precht@gmail.com>
2018-12-24 22:58:41 +0100

    refactor: use crate_authors and crate_version macros

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,16 +1,12 @@
+#[macro_use]
 extern crate clap;
-extern crate toml;
 
 use clap::App;
 
 fn main() {
-  let pkg: toml::Value = toml::from_str(include_str!("../Cargo.toml")).unwrap();
-  let version = pkg["package"]["version"].as_str().unwrap();
-  let authors = pkg["package"]["authors"].as_array().unwrap();
-
-  App::new("Vibranium CLI")
-          .version(version)
-          .author(authors[0].as_str().unwrap())
-          .about("Building DApps made easy")
-          .get_matches();
+  let matches = App::new("Vibranium CLI")
+                  .version(crate_version!())
+                  .author(crate_authors!())
+                  .about("Building DApps made easy")
+                  .get_matches();
 }
